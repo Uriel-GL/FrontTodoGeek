624c8ba3b2e31d2cf790faa2af98559ad2d99ec0
Diego <arciniegagd@hotmail.com>
2023-11-06 20:25:11 -0600

    Primer diseño del apartado de animes y series.

---
+++ src/views/Series.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-card v-for="serie in series" :key="serie.Id">
+            <v-card-title>{{ serie.Title }}</v-card-title>
+            <v-card-subtitle>{{ serie.Descripcion }}</v-card-subtitle>
+            <v-card-item>{{ serie.Episodios }}</v-card-item>
+            <v-img>{{ serie.Imagen }}</v-img>
+        </v-card>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            series: []
+        }
+    },
+    mounted(){
+        axios.get('')
+        .then(response => {
+            this.series = response.data;
+        })
+        .catch(error => {
+            console.error('No se pudieron cargar los datos:', error)
+        });
+    }
+}
+</script>
