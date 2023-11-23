8167c7e796ba1928d47014cd78d88a10f5eb684f
Uriel Gonzalez Lugo <gonzalezlugou@gmail.com>
2023-11-23 00:11:32 -0600

    Integración del cache dinamico en el sw.js

--- sw.js
+++ sw.js
@@ -22,11 +22,47 @@ self.addEventListener('activate', (event) => {
     return self.clients.claim();
 });
 
+//fetch
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
 
+    // Para contenido estático
+    if (event.request.method === 'GET' && event.request.url.match(/\.(js|png|jpg|html|vue)$/)) {
+        event.respondWith(
+            caches.open('static').then((cache) => {
+                return cache.match(event.request).then((response) => {
+                    return response || fetch(event.request).then((newResponse) => {
+                        cache.put(event.request, newResponse.clone());
+                        return newResponse;
+                    });
+                });
+            })
+        );
+    }
+
+    // Para contenido dinámico de la API
+    else if (event.request.url.startsWith('https://localhost:7121/api')) {
+        event.respondWith(
+            caches.open('apiCache').then((cache) => {
+                return cache.match(event.request).then((response) => {
+                    return response || fetch(event.request).then((newResponse) => {
+                        cache.put(event.request, newResponse.clone());
+                        return newResponse;
+                    });
+                });
+            })
+        );
+    }
+
+    else {
+        event.respondWith(
+            caches.open('static').then((cache) => {
+                return cache.match(event.request).then((response) => {
+                    return response || fetch(event.request).then((newResponse) => {
+                        cache.put(event.request, newResponse.clone());
+                        return newResponse;
+                    });
+                });
+            })
+        );
+    }
+});
