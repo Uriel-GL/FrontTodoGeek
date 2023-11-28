4266708e7ec6844d7a4a02ad477f697b29c9c6ba
Uriel Gonzalez Lugo <gonzalezlugou@gmail.com>
2023-11-27 18:04:32 -0600

    Cambios que ni son tan cambios por mi cache-URI.

---
+++ src/views/JuegosView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="bodyAnimes">
+
+        <v-row class="mt-4">
+            <v-col v-for="anime in animes" :key="anime.id" cols="6" xxl="3" xl="3" lg="3" md="3" sm="3" xs="6">
+                <CardComponent :data="anime"></CardComponent>
+            </v-col>
+        </v-row>
+    </div>
+    <div v-if="isLoading">
+        <v-row>
+            <v-col v-for="n in 4" :key="n" cols="6" xxl="3" xl="3" lg="3" md="3" sm="3" xs="6">
+                <v-skeleton-loader
+                    class="mx-auto border"
+                    max-width="240"
+                    max-height="370"
+                    type="card-avatar, actions"
+                ></v-skeleton-loader>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+<script>
+import ColorsApp from '../js/AppColors';
+import { VSkeletonLoader } from 'vuetify/lib/labs/components.mjs';
+import CardComponent  from '../components/CardVComponent.vue'
+import axios from "axios";  
+export default {
+    data(){
+        return{
+            colors: ColorsApp,
+            isLoading: false,
+            animes: []
+        }
+    },
+    components: {
+        VSkeletonLoader,
+        CardComponent
+    },
+    mounted(){
+        axios.get('https://localhost:7121/api/Game/Resena')
+        .then(response => {
+            this.animes = response.data;
+        })
+        .catch(error => {
+            console.error('No se pudieron cargar los datos:', error)
+        });
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.bodyAnime {
+  padding: 15px;
+}
+</style>
